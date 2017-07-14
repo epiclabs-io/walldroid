a8913f2182c95594d89edcba880ec59b6a95b7e1
Leandro Moreno <leandro@epiclabs.io>
2017-07-14 12:04:21 +0200

    Adding new plugins button working

--- app/src/main/java/io/epiclabs/walldroid/core/PluginManager.java
+++ app/src/main/java/io/epiclabs/walldroid/core/PluginManager.java
@@ -13,22 +13,14 @@ public class PluginManager {
     public static final List<Plugin> plugins = new ArrayList<Plugin>();
     public static final Map<String, Plugin> pluginsMap = new HashMap<String, Plugin>();
 
-    private static final int COUNT = 3;
-
-    static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createPlugin(i));
-        }
+    public static void newPlugin(Plugin plugin) {
+        addItem(plugin);
     }
 
-    private static void addItem(Plugin item) {
+    public static void addItem(Plugin item) {
         plugins.add(item);
         pluginsMap.put(item.getAlias(), item);
     }
 
-    private static Plugin createPlugin(int position) {
-        return new JiraPlugin("alias " + position, "host" + position + "00", "username", "password", "wallboard id", 123, "effect string", false);
-    }
 
 }
